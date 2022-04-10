f8d91080a5631302b69adedb9a75ec328f908b8f
ecolytiq <mirko.kaempf@ecolytiq.com>
2022-04-10 20:59:01 +0200

    added initial results to main page

--- pyGeoQB/script/bootstrap.sh
+++ pyGeoQB/script/bootstrap.sh
@@ -3,7 +3,6 @@
 pip install --upgrade pip
 pip install --upgrade -q gspread
 pip install googlemaps polyline
-pip install google-colab
 pip install mysql-connector-python sqlalchemy pymysql
 pip install networkx
 pip install node2vec
