61ce313bfd598c4c032f74273896bbe7c16f7f9b
ecolytiq <mirko.kaempf@ecolytiq.com>
2022-04-12 14:51:13 +0200

    fixed scripts + allias

--- pyGeoQB/script/bootstrap.sh
+++ pyGeoQB/script/bootstrap.sh
@@ -9,3 +9,6 @@ source $1/bin/activate
 pip install --upgrade pip
 pip install --upgrade -q gspread
 pip install -r requirements.txt
+
+alias gqws='source env/env.sh $1; python3 examples/gqws.py'
+alias gql='source env/env.sh $1; python3 examples/gql.py'
