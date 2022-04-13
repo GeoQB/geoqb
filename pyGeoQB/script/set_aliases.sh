993bfd5a6090899c8fca2f8ccfde941ffc0c1982
ecolytiq <mirko.kaempf@ecolytiq.com>
2022-04-13 08:34:23 +0200

    updated alias scritp

---
+++ pyGeoQB/script/set_aliases.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" -eq "" ]
+then
+   envV=env1
+else
+   envV=$1
+fi
+clear
+echo "********************************************************************************************"
+echo "* Aliases for GeoQB commands will be defined for the virtual environment: $envV"
+echo "********************************************************************************************"
+alias gqws="source env/env.sh $envV; python3 examples/gqws.py"
+alias gql="source env/env.sh $envV; python3 examples/gql.py"
+
+alias
